4035f69b5fa1cde8fb2d99b2f86c0a285ce8d730
Anastasia Tsiklauri <anastasia_tsiklauri@yahoo.com>
2026-05-03 23:02:28 +0400

    all recipes page + fixed navbar paths

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,81 @@
-fetch('./components/navbar.html')
+// fix paths
+function getPath(path) {
+    const isInPages = window.location.pathname.includes('/pages/');
+    return isInPages ? `../${path}` : `./${path}`;
+}
+  
+// navbar
+fetch(getPath('components/navbar.html'))
 .then(response => response.text())
 .then(data => {
-  document.querySelector('#navbar').innerHTML = data;
+    const navbar = document.querySelector('#navbar');
+    if (navbar) navbar.innerHTML = data;
 });
-
-fetch('./components/footer.html')
+  
+// footer
+fetch(getPath('components/footer.html'))
 .then(response => response.text())
 .then(data => {
-  document.querySelector('#footer').innerHTML = data;
+    const footer = document.querySelector('#footer');
+    if (footer) footer.innerHTML = data;
 });
+  
+// cards featured
+const HomeContainer = document.querySelector('#home-container');
+if (HomeContainer) {
+    fetch(getPath('datas/data.json'))
+    .then(response => response.json())
+    .then(recipes => {
+        const featuredRecipes = recipes.filter(recipe => recipe.featured);
 
-const container = document.querySelector('#recipe-container');
-
-fetch('./datas/data.json')
-.then(response => response.json())
-.then(recipes => {
-    recipes.forEach(recipe => {
+        featuredRecipes.forEach(recipe => {
         const card = document.createElement('div');
         card.className = 'col-md-6 col-lg-3';
 
         card.innerHTML = `
-        <div class="recipe-card">
-            <div class="recipe-img"></div>
-            <h4>${recipe.name}</h4>
-            <div class="d-flex gap-2 flex-wrap">
-            <span class="recipe-tag">
-                ${recipe.time}
-            </span>
-            <span class="recipe-tag">
-                ${recipe.difficulty}
-            </span>
+            <div class="recipe-card">
+                <div class="recipe-img"></div>
+                <h4>${recipe.name}</h4>
+                <div class="d-flex gap-2 flex-wrap">
+                <span class="recipe-tag">${recipe.time}</span>
+                <span class="recipe-tag">${recipe.difficulty}</span>
+                </div>
             </div>
-        </div>
         `;
 
-        container.appendChild(card);
-    });
-})
-.catch(error => console.log(error));
+        HomeContainer.appendChild(card);
+        });
+      })
+      .catch(error => console.log(error));
+};
+
+// all recipes cards
+const allRecipesContainer = document.querySelector('#recipe-container');
+if (allRecipesContainer) {
+    fetch(getPath('datas/data.json'))
+      .then(response => response.json())
+      .then(recipes => {
+        recipes.forEach(recipe => {
+          const card = document.createElement('div');
+          card.className = 'col-md-6 col-lg-3';
+  
+          card.innerHTML = `
+          <div class="recipe-card">
+              <div class="recipe-img"></div>
+              <h4>${recipe.name}</h4>
+              <div class="d-flex gap-2 flex-wrap">
+                <span class="recipe-tag">
+                  ${recipe.time}
+                </span>
+                <span class="recipe-tag">
+                  ${recipe.difficulty}
+                </span>
+              </div>
+          </div>
+          `;
+  
+          allRecipesContainer.appendChild(card);
+        });
+      })
+      .catch(error => console.log(error));
+  }
