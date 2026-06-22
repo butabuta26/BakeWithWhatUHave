ac141c6eb42a704ac878c3b794f68259097c30ea
Anastasia Tsiklauri <147313565+butabuta26@users.noreply.github.com>
2026-06-22 16:56:55 +0400

    working mongodb

--- passport-config.js
+++ passport-config.js
@@ -5,24 +5,26 @@ const bcrypt = require("bcrypt")
 function initialize(passport, getUserByEmail, getUserById){
     // Function to authenticate users
     const authenticateUsers = async (email, password, done) => {
-        // Get users by email
-
-        const user = getUserByEmail(email)
-        
-        if (user == null){
-            return done(null, false, {message: "No user found with that email"})
-        }
         try {
-            if(await bcrypt.compare(password, user.password)){
-                return done(null, user)
-            } else{
-                return done (null, false, {message: "Password Incorrect"})
+            const user = await getUserByEmail(email);
+    
+            if (!user) {
+                return done(null, false, { message: "No user found with that email" });
             }
+    
+            const match = await bcrypt.compare(password, user.password);
+    
+            if (match) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: "Password Incorrect" });
+            }
+    
         } catch (e) {
             console.log(e);
-            return done(e)
+            return done(e);
         }
-    }
+    };
 
     passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUsers))
     passport.serializeUser((user, done) => done(null, user.id))
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+console.log("=== THIS IS MY SERVER.JS ===");
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
@@ -21,12 +22,28 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connection successful"))
   .catch(err => console.error(err));
 
-const users = [];
+const userSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String
+})
 
+const users = mongoose.model("User", userSchema)
+
+// const users = [];
+
+// array version 
+// initializePassport(
+//     passport,
+//     email => users.find(user => user.email === email),
+//     id => users.find(user => user.id === id)
+// );
+
+// mongo version
 initializePassport(
     passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
+    async email => await users.findOne({ email }),
+    async id => await users.findById(id)
 );
 
 // View engine
@@ -70,19 +87,39 @@ app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
     })
 );
 
-// Registration
+// Registration (array version)
+// app.post("/register", checkNotAuthenticated, async (req, res) => {
+//     try {
+//         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+//         users.push({
+//             id: Date.now().toString(),
+//             name: req.body.name,
+//             email: req.body.email,
+//             password: hashedPassword
+//         });
+
+//         console.log(users);
+//         res.redirect("/login");
+//     } catch (err) {
+//         console.error(err);
+//         res.redirect("/register");
+//     }
+// });
+
+// mongo version
 app.post("/register", checkNotAuthenticated, async (req, res) => {
+    // console.log("Register route hit!");
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-        users.push({
-            id: Date.now().toString(),
+        const newUser = await users.create({
             name: req.body.name,
             email: req.body.email,
             password: hashedPassword
         });
+        // console.log("Saved user:", newUser);
 
-        console.log(users);
         res.redirect("/login");
     } catch (err) {
         console.error(err);
